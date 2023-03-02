7e4851be320e903b710d497514e497cdbfa46939
embsu <122359976+embsu@users.noreply.github.com>
2023-03-02 14:43:38 +0200

    Delete database.js

--- database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-const connection = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'netuser',
-  password: 'netpass',
-  database: 'netdb'
-});
-module.exports = connection;
