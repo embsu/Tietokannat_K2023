410006d086878a1c4066faccf4ae851d14f6c4af
embsu <122359976+embsu@users.noreply.github.com>
2023-03-02 15:18:29 +0200

    Add files via upload

---
+++ Viikko7/Kotiteht17/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var arviointiRouter = require('./routes/arviointi');
+//var aopintojaksoRouter = require('./routes/opintojakso');
+//var opiskelijaRouter = require('./routes/opiskelija');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/arviointi', arviointiRouter);
+//app.use('/opintojakso', opintojaksoRouter);
+//app.use('/opiskelija', opiskelijaRouter);
+
+module.exports = app;
